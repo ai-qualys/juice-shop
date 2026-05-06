1c6520d7bc1f1ac44d1c81988c1f9f4de992849c
ai-qualys <aiyengar@qualys.com>
2026-05-05 23:01:25 -0700

    security: fix CWE-79 in frontend/src/app/administration/administration.component.ts

--- frontend/src/app/administration/administration.component.ts
+++ frontend/src/app/administration/administration.component.ts
@@ -10,7 +10,7 @@ import { FeedbackService } from '../Services/feedback.service'
 import { MatTableDataSource, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table'
 import { UserService } from '../Services/user.service'
 import { Component, type OnInit, ViewChild, inject } from '@angular/core'
-import { DomSanitizer } from '@angular/platform-browser'
+import { DomSanitizer, SecurityContext } from '@angular/platform-browser'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faArchive, faEye, faHome, faTrashAlt, faUser } from '@fortawesome/free-solid-svg-icons'
 import { MatPaginator } from '@angular/material/paginator'
@@ -75,7 +75,7 @@ export class AdministrationComponent implements OnInit {
       next: (feedbacks) => {
         this.feedbackDataSource = feedbacks
         for (const feedback of this.feedbackDataSource) {
-          feedback.comment = this.sanitizer.bypassSecurityTrustHtml(feedback.comment)
+          feedback.comment = this.sanitizer.sanitize(SecurityContext.HTML, feedback.comment)
         }
         this.feedbackDataSource = new MatTableDataSource(this.feedbackDataSource)
         this.feedbackDataSource.paginator = this.paginatorFeedb
